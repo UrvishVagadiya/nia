dd42c4f128e4d8f000d534b8fe462c3e18bc6679
Urvish Vagdiya <urvish@gmail.com>
2026-05-15 16:16:25 +0530

    mobile number

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -76,9 +76,13 @@ export default buildConfig({
     transportOptions: {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     },
   }),
